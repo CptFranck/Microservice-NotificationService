a91f5ed93731c2c6b8c9e451c2166ced6e5db2e7
CptFranck <francois.caillet71@gmail.com>
2025-10-10 15:52:24 +0200

    update BookingRequestListener

---
+++ src/main/java/com/CptFranck/NotificationService/listener/BookingRequestListener.java
@@ -0,0 +1,38 @@
+package com.CptFranck.NotificationService.listener;
+
+import com.CptFranck.dto.BookingConfirmed;
+import com.CptFranck.dto.BookingEvent;
+import com.CptFranck.dto.BookingRejected;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import com.CptFranck.NotificationService.service.NotificationService;
+
+@Component
+@Slf4j
+public class BookingRequestListener {
+
+    private final NotificationService notificationService;
+
+    public BookingRequestListener(NotificationService notificationDispatcherService) {
+        this.notificationService = notificationDispatcherService;
+    }
+
+    @KafkaListener(topics = "booking-event", groupId = "notification-service")
+    public void onBookingEvent(BookingEvent event) {
+        log.info("Received booking event: {}", event);
+        notificationService.send(event.getUserId().toString(), "/queue/bookings", "booking event", event);
+    }
+
+    @KafkaListener(topics = "booking-confirmed", groupId = "notification-service")
+    public void onBookingConfirmed(BookingConfirmed event) {
+        log.info("Received booking confirmed: {}", event);
+        notificationService.send(event.getUserId().toString(), "/queue/bookings", "booking event", event);
+    }
+
+    @KafkaListener(topics = "booking-rejected", groupId = "notification-service")
+    public void onBookingRejected(BookingRejected event) {
+        log.info("Received booking rejected: {}", event);
+        notificationService.send(event.getUserId().toString(), "/queue/bookings", "booking event", event);
+    }
+}
