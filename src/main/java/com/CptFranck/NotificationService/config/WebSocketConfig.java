bd12e5239c543f6ee138598b2423d9be6a0908da
CptFranck <francois.caillet71@gmail.com>
2025-10-16 16:41:15 +0200

    update WebSocketConfig

--- src/main/java/com/CptFranck/NotificationService/config/WebSocketConfig.java
+++ src/main/java/com/CptFranck/NotificationService/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-notification")
-                .setAllowedOriginPatterns("http://localhost:63342")
+                .setAllowedOriginPatterns("http://localhost:63342", "http://localhost:8090")
                 .withSockJS();
     }
 
